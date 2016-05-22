ebfcd5000abba5ed4babd05f3cdd4b9c78be43f8
Mark Bradley <mbradleymark@gmail.com>
2016-05-22 10:40:10 +0100

    Added connection to internal Database

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,31 @@
-import { Template } from 'meteor/templating';
- 
-import './body.html';
- 
+import { Template } from "meteor/templating";
+
+import { Tasks } from "../api/tasks.js";
+
+import "./body.html";
+
 Template.body.helpers({
-  tasks: [
-    { text: 'This is dummy task 1' },
-    { text: 'This is dummy task 2' },
-    { text: 'This is dummy task 3' },
-  ],
+    tasks() {
+        return Tasks.find({});
+    }
 });
 
+Template.body.events({
+    "submit .new-task" (event) {
+        console.log ("In add task event")
+        // don't let browser do submit
+        event.preventDefault();
+
+        // get data from form
+       const target=event.target;
+        const text=target.text.value;
+
+        // then insert into db
+        Tasks.insert({
+            text,                   // value from form
+            createdAt: new Date(), // mark with timestamp
+        });
+        // clear input
+        target.text.value="";
+    }, //add comma here if adding more
+});

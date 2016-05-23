695d3d443559519e5b826376693c2074bae98acf
Mark Bradley <mbradleymark@gmail.com>
2016-05-23 12:26:41 +0100

    Added completed status and hide function

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,30 +1,48 @@
 import { Template } from "meteor/templating";
+import { ReactiveDict } from "meteor/reactive-dict";
 
 import { Tasks } from "../api/tasks.js";
 
 import "./task.js";
 import "./body.html";
 
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
-    tasks() {   // Get all task. group by user, most recent first
-        return Tasks.find ( {}, { sort: { userName : 1, createdAt : -1} } );
+    tasks() {
+        const instance = Template.instance();
+
+        if (instance.state.get("hide-completed")) { //don't show complered things
+            return Tasks.find({ completed: { $ne: true }}, {sort: { userName: 1, createdAt: -1} });
+        } else {
+            // Get all task. group by user, most recent first
+            return Tasks.find({}, { sort: { userName: 1, createdAt: -1 } });
+        }
     }
 });
 
 Template.body.events({
+    "change .hide-completed" (event, instance) {
+        instance.state.set("hide-completed", event.target.checked);
+    },
+
     "submit .new-task" (event) {
         event.preventDefault();
 
         // get data from form
-        const form=event.target;
-        const text=form.text.value;
+        const form = event.target;
+        const text = form.text.value;
+
 
         // then insert into db
         Tasks.insert({
-            text,                   // value from form
+
+            text, // value from form
             createdAt: new Date() // mark with timestamp
         });
         // clear input
-        form.text.value="";
-    }   //add comma here if adding more
+        form.text.value = "";
+    } //add comma here if adding more
 });
--- imports/ui/task.js
+++ imports/ui/task.js
@@ -6,7 +6,7 @@ import "./task.html";
 
 Template.task.events({
     "click .toggle-checked" () { // set checked to opposite on current task
-        Tasks.update ( this._id, { $set: { checked : ! this.checked }
+        Tasks.update ( this._id, { $set: { completed : ! this.completed }
         });
     },
     "click .delete" () { // delete current task ( this )
